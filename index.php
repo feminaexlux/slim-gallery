18ff9e00453e7209f6eadd020cc3d7561cbb6fcd
Lani Aung <github@laniaung.me>
2015-03-08 12:03:13 -0700

    Adding template

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
-<!doctype html>
-<html>
 <?php
 
 require_once 'vendor/autoload.php';
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim(array(
-    'debug' => true
+    'debug' => true,
+    'templates.path' => './view'
 ));
+
+try {
+    $conn = new PDO("mysql:host=192.168.0.108;dbname=gallery", 'gallery', 'gallery');
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    $log = $app->getLog();
+    $log->error($e->getMessage());
+}
 
 $app->get('/:name', function ($name) {
     echo "parameter $name";
 });
 
-$app->get('/', function () {
-    echo <<<EOT
-    <title>Gallery</title>
-    <body>
-        This is a gallery?
-    </body>
-EOT;
+$app->get('/', function () use ($app) {
+    $app->render('base.php',
+        array(
+            'title' => 'Gallery',
+            'content' => 'Dynamic content I guess?'
+        )
+    );
 });
 
-$app->run(); ?>
-
-</html>
+$app->run();
---
+++ view/base.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html lang="en">
+    <title><?=$title?></title>
+    <body>
+        <p><?=$content?></p>
+    </body>
+</html>
